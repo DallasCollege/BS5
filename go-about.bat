cbc731a7786ce26fea5211ecbaca1dad9f4b29a9
rscottdc <rscott@dcccd.edu>
2023-09-11 11:09:55 -0500

    initial BS3

--- go-about.bat
+++ go-about.bat
@@ -1,4 +1,4 @@
 @echo off
-for /D %%a in ("C:\OneDrive - Dallas College\_github\BS5\about\*.*") do xcopy /y /d "C:\OneDrive - Dallas College\_github\BS5\default.html" "%%a\"
+for /D %%a in ("C:\OneDrive - Dallas College\_github\BS5\about\*.*") do xcopy /y /d "C:\OneDrive - Dallas College\_github\BS5\index.html" "%%a\"
 
 
